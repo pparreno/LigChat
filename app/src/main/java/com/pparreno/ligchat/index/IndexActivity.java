8c0b6082f9e7c2e3efa0058df81d7586c284e8cf
Paul John B. Parreno <paul.john.parreno@gmail.com>
2020-08-06 22:36:28 +0800

    renaming existing mainactivity class and files to indexactivity

--- app/src/main/java/com/pparreno/ligchat/index/IndexActivity.java
+++ app/src/main/java/com/pparreno/ligchat/index/IndexActivity.java
@@ -1,14 +1,16 @@
-package com.pparreno.ligchat;
+package com.pparreno.ligchat.index;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pparreno.ligchat.R;
+
+public class IndexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
     }
 }
