c0308abd1e0b7b4e0ea2dfc70dd8f508c48fbff0
Paul John B. Parreno <paul.john.parreno@gmail.com>
2020-08-07 08:49:35 +0800

    commiting final stylizing of the index screen

---
+++ app/src/main/java/com/pparreno/ligchat/views/ArialButton.java
@@ -0,0 +1,26 @@
+package com.pparreno.ligchat.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class ArialButton extends androidx.appcompat.widget.AppCompatButton {
+    public ArialButton(Context context) {
+        super(context);
+    }
+
+    public ArialButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ArialButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void applyCustomFont(Context context) {
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "arial_mt.ttf");
+        this.setTypeface(typeface);
+    }
+
+}
