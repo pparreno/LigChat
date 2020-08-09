0620829152bef674ed1d80876d9c2b71aa9e4456
Paul John B. Parreno <paul.john.parreno@gmail.com>
2020-08-09 12:57:59 +0800

    Included timber into the project

--- app/src/main/java/com/pparreno/ligchat/onboarding/viewmodel/SignUpViewModel.java
+++ app/src/main/java/com/pparreno/ligchat/onboarding/viewmodel/SignUpViewModel.java
@@ -1,7 +1,25 @@
 package com.pparreno.ligchat.onboarding.viewmodel;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.pparreno.ligchat.onboarding.repository.SignUpRepository;
+
 public class SignUpViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<FirebaseUser> user;
+
+    private SignUpRepository signUpRepository;
+
+    public SignUpViewModel() {
+        this.signUpRepository = new SignUpRepository();
+    }
+
+    public void RegisterUser(@NonNull String username, @NonNull String email, @NonNull String password)
+    {
+
+    }
+
 }
